7398838d0e34c8f52500d8ea84df7d019667cf6a
rluzuriaga <rodrigoluzuriaga@icloud.com>
2020-05-08 22:29:23 -0700

    Add view code to render the form. Once the form is submitted, a printer object is created in the database.

--- Open_Printer_Management_System/app/views.py
+++ Open_Printer_Management_System/app/views.py
@@ -1,4 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .forms import AddPrinterForm
+from .models import Printer
 
 def homepage(request):
-    return render(request, 'app/home.html')
+    if request.method == 'POST':
+        form = AddPrinterForm(request.POST)
+
+        if form.is_valid():
+            RunFormFunction(form)
+
+        return redirect('homepage')
+    else:
+        form = AddPrinterForm()
+
+    return render(request, 'app/home.html', {'form': form})
+
+def RunFormFunction(form):
+    printer_name = form.cleaned_data['printer_name']
+    printer_model_name = form.cleaned_data['printer_model_name']
+    printer_location = form.cleaned_data['printer_location']
+    ip_address = form.cleaned_data['ip_address']
+    department_name = form.cleaned_data['department_name']
+
+    Printer.objects.create(
+        printer_name=printer_name,
+        printer_model_name=printer_model_name,
+        printer_location=printer_location,
+        ip_address=ip_address,
+        department_name=department_name
+    )
