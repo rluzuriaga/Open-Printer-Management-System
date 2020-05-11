8d9caf546fbf6bd6acb28de310892c38724fc3af
rluzuriaga <rodrigoluzuriaga@icloud.com>
2020-05-11 16:01:47 -0700

    Register TonerLevel to admin site

--- Open_Printer_Management_System/app/admin.py
+++ Open_Printer_Management_System/app/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from .models import Printer
+from .models import Printer, TonerLevel
 
 class PrinterAdmin(admin.ModelAdmin):
     list_display = ('printer_name', 'printer_model_name', 'printer_location', 'ip_address', 'department_name')
 
 admin.site.register(Printer, PrinterAdmin)
+
+class TonerLevelAdmin(admin.ModelAdmin):
+    list_display = ('printer_name', 'date_time', 'module_identifier', 'level')
+
+admin.site.register(TonerLevel, TonerLevelAdmin)
