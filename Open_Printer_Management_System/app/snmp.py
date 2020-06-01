096d1c36649dc0c1636180fe5e19cf83b4dd9a82
rluzuriaga <rodrigoluzuriaga@icloud.com>
2020-06-01 16:02:07 -0700

    Add snmp code for the last commit

--- Open_Printer_Management_System/app/snmp.py
+++ Open_Printer_Management_System/app/snmp.py
@@ -3,24 +3,21 @@
 
 import re
 from easysnmp import Session
-from easysnmp.exceptions import EasySNMPTimeoutError
+from easysnmp.exceptions import EasySNMPTimeoutError, EasySNMPConnectionError
 
 class SNMP():
-    def __init__(self, hostname):
+    def __init__(self, hostname, version):
         self.hostname = hostname
+        self.version = version
 
-        self.session = Session(hostname=self.hostname, community='public', version=3)
+        self.session = Session(hostname=self.hostname, community='public', version=self.version)
 
     def get_consumable_levels(self):
         try:
             consumable_number = len(self.session.walk(".1.3.6.1.2.1.43.11.1.1.6.1"))
-        except (SystemError, EasySNMPTimeoutError):
-            try:
-                self.session = Session(hostname=self.hostname, community='public', version=2)
-                consumable_number = len(self.session.walk(".1.3.6.1.2.1.43.11.1.1.6.1"))
-            except (SystemError, EasySNMPTimeoutError):
-                self.session = Session(hostname=self.hostname, community='public', version=1)
-                consumable_number = len(self.session.walk(".1.3.6.1.2.1.43.11.1.1.6.1"))
+        except (SystemError, EasySNMPTimeoutError, EasySNMPConnectionError):
+            res = {"Printer seems to be off": "Not on"}
+            return res                    
 
         res = dict()
 
@@ -78,3 +75,39 @@ class SNMP():
                 levels_dict[module_name] = "{:.0f}".format(level_percentage)
 
         return levels_dict
+
+def determine_snmp_version(hostname):
+    version = -1
+    try:
+        session = Session(hostname=hostname, community='public', version=3)
+        description = session.get('.1.3.6.1.2.1.1.1.0')
+        version = 3
+    except SystemError as error:
+        if "returned NULL without setting an error" in str(error):
+            try:
+                session = Session(hostname=hostname, community='public', version=2)
+                description = session.get('.1.3.6.1.2.1.1.1.0')
+                version = 2
+            except SystemError as error:
+                if "returned NULL without setting an error" in str(error):
+                    try:
+                        session = Session(hostname=hostname, community='public', version=1)
+                        description = session.get('.1.3.6.1.2.1.1.1.0')
+                        version = 1
+                    except SystemError:
+                        return -1
+                else:
+                    return -2
+        else:
+            return -2
+    
+    return version
+
+def determine_printer_model(hostname, version):
+    session = Session(hostname=hostname, community='public', version=version)
+    try:
+        printer_model_name = session.get('.1.3.6.1.2.1.25.3.2.1.3.1').value
+    except SystemError:
+        return -1
+    
+    return printer_model_name
