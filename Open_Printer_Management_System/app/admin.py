1b4bd86ebc928e4a9b825d5e3f74b3ee8e82891e
rluzuriaga <rodrigoluzuriaga@icloud.com>
2020-06-22 16:06:59 -0700

    Register new PrinterModel model to the admin site.

--- Open_Printer_Management_System/app/admin.py
+++ Open_Printer_Management_System/app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Printer, TonerLevel
+from .models import Printer, TonerLevel, PrinterModel
 
 class PrinterAdmin(admin.ModelAdmin):
     list_display = ('printer_name', 'printer_model_name', 'printer_location', 'ip_address', 'department_name')
@@ -11,3 +11,8 @@ class TonerLevelAdmin(admin.ModelAdmin):
     list_display = ('printer_name', 'date_time', 'module_identifier', 'level')
 
 admin.site.register(TonerLevel, TonerLevelAdmin)
+
+class PrinterModelAdmin(admin.ModelAdmin):
+    list_display = ("model_name", "module_numbers")
+
+admin.site.register(PrinterModel, PrinterModelAdmin)
